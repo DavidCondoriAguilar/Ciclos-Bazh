cd053abf3df2a307a7ceeb34041666963fb6c3cb
natvvgls <natalia.d.m975@gmail.com>
2023-09-12 12:25:39 -0500

    navbar listo

--- src/common/MainNav.jsx
+++ src/common/MainNav.jsx
@@ -4,31 +4,19 @@ import Instagram from "./../../public/images/instagram.jpg";
 import Linkedin from "./../../public/images/linkedin.jpg";
 
 export default function MainNav() {
-  return (
-    <>
+   return (
       <header className="bg-black py-4">
-        <nav className="container flex justify-between items-center">
-          <img  src={Logo} title="Logo Bazh" />
+         <nav className="w-full px-[4%]
+         xl:max-w-[1200px] xl:px-0 xl:mx-auto
+         flex justify-between items-center">
+            <img src={Logo} title="Logo Bazh" />
 
-          <ul className="flex gap-[24px]">
-            <li>
-              <a href="#">
-                <img src={Facebook} alt="" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src={Instagram} alt="" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src={Linkedin} alt="" />
-              </a>
-            </li>
-          </ul>
-        </nav>
+            <div className="flex gap-[24px]">
+               <img src={Facebook} alt="" />
+               <img src={Linkedin} alt="" />
+               <img src={Instagram} alt="" />
+            </div>
+         </nav>
       </header>
-    </>
-  );
+   );
 }
